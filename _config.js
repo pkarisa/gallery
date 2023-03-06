e0868dec6aa8a138109e9f5e07863a58070a0dc1
Pendo Karisa <114084573+pkarisa@users.noreply.github.com>
2023-03-06 22:07:42 +0300

    testing

--- _config.js
+++ _config.js
@@ -2,7 +2,7 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://karisa:07122323@darkroom.q5wfsfa.mongodb.net/test',
+    production: 'mongodb+srv://fkarisa:07122323@darkroom.q5wfsfa.mongodb.net/test',
     development: 'mongodb+srv://fkarisa:07122323@darkroom.q5wfsfa.mongodb.net/test',
     test: 'mongodb+srv://fkarisa:07122323@darkroom.q5wfsfa.mongodb.net/test',
 }
