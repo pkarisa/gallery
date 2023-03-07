44a948467e7f6ff484fa5dd6c0bc2a580a2c018b
pkarisa <faith.pendo@student.moringaschool.com>
2023-03-07 15:56:39 +0300

    update

--- _config.js
+++ _config.js
@@ -2,8 +2,8 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://fkarisa:07122323@darkroom.q5wfsfa.mongodb.net/test',
-    development: 'mongodb+srv://fkarisa:07122323@darkroom.q5wfsfa.mongodb.net/test',
-    test: 'mongodb+srv://fkarisa:07122323@darkroom.q5wfsfa.mongodb.net/test',
+    production: 'mongodb+srv://fkarisa:07122323@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://fkarisa:07122323@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://fkarisa>:07122323@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
